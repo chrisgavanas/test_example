221f3058aeb3ddaaa4dbf1f01af6df525b79b83e
Chris Gavanas <chrisgavanas@di.uoa.gr>
2018-11-24 13:26:49 +0200

    Added an extra println and a new static method win Hello class

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,12 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        System.out.println("Hello World 2");
+    }
+
+    public static void foo() {
+
     }
 
 }
